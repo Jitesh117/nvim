8522f8025161f83979ad878ad479b5bb0d69ee97
Jitesh117 <sahookumarjitesh@gmail.com>
2024-10-02 19:43:15 +0530

    update: new nvchad ui update to chadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,7 +4,7 @@
 
 local options = {
   base46 = {
-    theme = "onedark",
+    theme = "tokyonight",
     -- transparency = true,
     hl_override = {
       NvDashAscii = {
@@ -17,19 +17,45 @@ local options = {
       },
     },
   },
-  ui = {
-    nvdash = {
-      load_on_startup = true,
-      header = {
-        [[                                                        ]],
-        [[ ██╗   ██╗███████╗ ██████╗ ██████╗ ██████╗ ███████╗    ]],
-        [[ ██║   ██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝    ]],
-        [[ ██║   ██║███████╗██║     ██║   ██║██║  ██║█████╗      ]],
-        [[ ╚██╗ ██╔╝╚════██║██║     ██║   ██║██║  ██║██╔══╝      ]],
-        [[  ╚████╔╝ ███████║╚██████╗╚██████╔╝██████╔╝███████╗    ]],
-        [[   ╚═══╝  ╚══════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ]],
-        [[                                                        ]],
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      [[                                                        ]],
+      [[ ██╗   ██╗███████╗ ██████╗ ██████╗ ██████╗ ███████╗    ]],
+      [[ ██║   ██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝    ]],
+      [[ ██║   ██║███████╗██║     ██║   ██║██║  ██║█████╗      ]],
+      [[ ╚██╗ ██╔╝╚════██║██║     ██║   ██║██║  ██║██╔══╝      ]],
+      [[  ╚████╔╝ ███████║╚██████╗╚██████╔╝██████╔╝███████╗    ]],
+      [[   ╚═══╝  ╚══════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ]],
+      [[                                                        ]],
+    },
+    buttons = {
+      {
+        txt = "  Restore Session",
+        keys = "Spc q s",
+        cmd = function()
+          require("persistence").load()
+        end,
       },
+      { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+      { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+      { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
+      { txt = "󱥚  Themes", keys = "Spc t h", cmd = "Telescope themes" },
+      { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashLazy",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
     },
   },
   mason = {
