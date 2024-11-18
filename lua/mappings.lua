658248b9adf2bd7b9ba2d06f0f623b8116cd8616
Jitesh117 <sahookumarjitesh@gmail.com>
2024-11-18 20:08:36 +0530

    feat: menu mappings, tab stuff and lazyLock

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -73,3 +73,17 @@ map("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Gitsigns Previous hunk" 
 -- leetcode mappings
 map("n", "<leader>lr", "<cmd>Leet run<CR>", { desc = "LeetCode Run tests" })
 map("n", "<leader>ls", "<cmd>Leet submit<CR>", { desc = "LeetCode Submit solution" })
+
+-- menu mappings
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open "default"
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set("n", "<RightMouse>", function()
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
+  require("menu").open(options, { mouse = true })
+end, {})
--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,14 @@ require "nvchad.options"
 
 local o = vim.opt
 o.relativenumber = true
+o.tabstop = 4
+o.shiftwidth = 4
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "html" },
+  callback = function()
+    vim.opt_local.expandtab = true
+  end,
+})
 require("gitsigns").setup {
   current_line_blame = true, -- Enable blame line by default
 }
