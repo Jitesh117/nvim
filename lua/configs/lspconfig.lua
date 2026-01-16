ab7c90272eb4729969a3eb613333e7291b5d44d0
Jitesh117 <sahookumarjitesh@gmail.com>
2026-01-16 09:46:54 +0530

    feat: html, tailwind suggestions on heex files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 local on_init = require("nvchad.configs.lspconfig").on_init
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "cssls" } -- removed html from here
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -20,7 +20,14 @@ end
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+-- HTML LSP with HEEx support
+lspconfig.html.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  filetypes = { "html", "heex" }, -- Add heex here
+}
+
 -- go LSP
 lspconfig.gopls.setup {
   on_attach = on_attach,
@@ -66,17 +73,65 @@ for _, lsp in ipairs(python_servers) do
   }
 end
 
-local web_dev_servers = { "ts_ls", "tailwindcss" }
+-- TypeScript LSP
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
 
-for _, lsp in ipairs(web_dev_servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
+-- Tailwind CSS LSP with HEEx support
+lspconfig.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {
+    "html",
+    "css",
+    "scss",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "heex",
+    "elixir",
+    "eelixir",
+  },
+  root_dir = function(fname)
+    local util = require("lspconfig.util")
+    return util.root_pattern("tailwind.config.js", "tailwind.config.ts", "assets/css/app.css", "mix.exs")(fname)
+  end,
+  init_options = {
+    userLanguages = {
+      elixir = "html-eex",
+      eelixir = "html-eex",
+      heex = "html-eex",
+    },
+  },
+  settings = {
+    tailwindCSS = {
+      includeLanguages = {
+        elixir = "html",
+        eelixir = "html",
+        heex = "html",
+      },
+      experimental = {
+        classRegex = {
+          [[class: "([^"]*)]],
+          [[class: '([^']*)]],
+          '~H""".*class="([^"]*)".*"""',
+          [[class="([^"]*)]],
+        },
+      },
+      validate = true,
+    },
+  },
+}
 
--- emmet lsp
+-- emmet lsp with HEEx support
 lspconfig.emmet_language_server.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
   filetypes = {
     "css",
     "eruby",
@@ -89,9 +144,8 @@ lspconfig.emmet_language_server.setup {
     "pug",
     "typescript",
     "typescriptreact",
+    "heex", -- Add heex here for Emmet support
   },
-  -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-  -- **Note:** only the options listed in the table are supported.
   init_options = {
     ---@type table<string, string>
     includeLanguages = {},
@@ -114,12 +168,6 @@ lspconfig.emmet_language_server.setup {
   },
 }
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
 -- c & cpp setup
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
@@ -130,3 +178,28 @@ lspconfig.clangd.setup {
   on_init = on_init,
   capabilities = capabilities,
 }
+
+-- elixir LSP (ElixirLS)
+lspconfig.elixirls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+
+  cmd = { "elixir-ls" },
+  filetypes = { "elixir", "eelixir", "heex" },
+
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = true,
+      fetchDeps = false, -- set true if you want auto deps fetching
+      enableTestLenses = true,
+      suggestSpecs = true,
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
